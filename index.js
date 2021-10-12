7fe9a11ecd97aa32a1c22673c139740ae1a7123b
BrellaH <33139496+BrellaH@users.noreply.github.com>
2021-10-12 18:05:28 -0400

    updated env var for port

--- index.js
+++ index.js
@@ -173,6 +173,8 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
